641178cb077dcd678bcb19ea66a30329164b5038
aryan-singh0001 <aaryansingh2203@gmail.com>
2026-05-22 12:01:33 +0530

    fixed api url

--- Frontend/src/features/auth/services/auth.api.js
+++ Frontend/src/features/auth/services/auth.api.js
@@ -9,7 +9,7 @@ const api = axios.create({
 export async function register({ username, email, password }) {
 
     try {
-        const response = await api.post('/api/auth/register', {
+        const response = await api.post(`${import.meta.env.VITE_API_URL}/api/auth/register`, {
             username, email, password
         })
 
@@ -27,7 +27,7 @@ export async function login({ email, password }) {
 
     try {
 
-        const response = await api.post("/api/auth/login", {
+        const response = await api.post(`${import.meta.env.VITE_API_URL}/api/auth/login`, {
             email, password
         })
 
@@ -42,7 +42,7 @@ export async function login({ email, password }) {
 export async function logout() {
     try {
 
-        const response = await api.get("/api/auth/logout")
+        const response = await api.get(`${import.meta.env.VITE_API_URL}/api/auth/logout`)
 
         return response.data
 
@@ -55,7 +55,7 @@ export async function getMe() {
 
     try {
 
-        const response = await api.get("/api/auth/get-me")
+        const response = await api.get(`${import.meta.env.VITE_API_URL}/api/auth/get-me`)
 
         return response.data
 
